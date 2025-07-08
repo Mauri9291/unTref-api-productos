5a4b7915607562aef12478537bda90b85f22f7d2
Mauricio Felippetti <mauri671@hotmail.com>
2025-07-07 23:41:26 -0300

    feat: Configura estructura inicial del proyecto y base de datos

--- models/producto.js
+++ models/producto.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose');
-
 const productoSchema = new mongoose.Schema({
   codigo: { type: String, required: true, unique: true },
   nombre: { type: String, required: true },
   precio: { type: Number, required: true },
   categoria: { type: [String], required: true }
 });
-
-const Producto = mongoose.model('Producto', productoSchema);
-module.exports = Producto;
+module.exports = mongoose.model('Producto', productoSchema);
